b003e1eb9517d240584382b30a51476935941b06
umeshwalagedara <umeshwalagedara@gmail.com>
2022-11-21 19:33:34 +0530

    test

--- src/main/java/com/example/students/UpdateStudentDetailsRequest.java
+++ src/main/java/com/example/students/UpdateStudentDetailsRequest.java
@@ -38,8 +38,8 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "", propOrder = {
     "studentDetails"
 })
-@XmlRootElement(name = "GetStudentDetailsResponse")
-public class GetStudentDetailsResponse {
+@XmlRootElement(name = "UpdateStudentDetailsRequest")
+public class UpdateStudentDetailsRequest {
 
     @XmlElement(name = "StudentDetails", required = true)
     protected StudentDetails studentDetails;
--- src/main/java/com/example/students/UpdateStudentDetailsResponse.java
+++ src/main/java/com/example/students/UpdateStudentDetailsResponse.java
@@ -38,8 +38,8 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "", propOrder = {
     "studentDetails"
 })
-@XmlRootElement(name = "GetStudentDetailsResponse")
-public class GetStudentDetailsResponse {
+@XmlRootElement(name = "UpdateStudentDetailsResponse")
+public class UpdateStudentDetailsResponse {
 
     @XmlElement(name = "StudentDetails", required = true)
     protected StudentDetails studentDetails;
